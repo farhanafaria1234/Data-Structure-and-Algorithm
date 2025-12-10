a109371b1e96610e5d7b7d2779311184c083d57a
farhanafaria1234 <farhanafaria1234@gmail.com>
2025-12-10 21:12:59 +0600

    Add files via upload

---
+++ Delete/Deleting last element.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int arr[10] = {5,10,15,20,25,30,35};
+    int n = 7;
+    //int k = 6;
+    //arr[0] = 5;
+    n--;
+    for(int i=0; i<n; i++)
+    {
+             cout << arr[i] << " ";
+
+    }
+
+
+    return 0;
+}
