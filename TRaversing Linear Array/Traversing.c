38bfd88014c1dee341b3f0fccc840273832a9b9c
farhanafaria1234 <farhanafaria1234@gmail.com>
2026-03-31 22:17:43 +0600

    Add files via upload

--- TRaversing Linear Array/Traversing.c
+++ TRaversing Linear Array/Traversing.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main()
+{
+    int k, LB, UB;
+    k=LB;
+    int arr[5]={1,2,3,4,5};
+    UB=4;
+    for(k=0; k<=UB; k++)
+    {
+        printf("%d \n", arr[k] );
+    }
+    return 0;
+}
