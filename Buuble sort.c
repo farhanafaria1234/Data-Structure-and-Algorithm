d72b801ff8a5522942ce372582d156456a25346f
farhanafaria1234 <farhanafaria1234@gmail.com>
2026-03-31 21:52:48 +0600

    Add files via upload

---
+++ Buuble sort.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10]={2,4,1,5,7};
+    int n=5;
+    int i, j, temp;
+    for(i=0; i<n-1; i++)
+    {
+        for(j=0; j<n-1-i; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
